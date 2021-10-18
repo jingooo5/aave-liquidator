0a898d04f38b38d8d82c26c4e78a362d8cf8fda5
jingooo5 <jingooo2@g.skku.edu>
2021-10-19 02:06:20 +0900

    update

---
+++ contracts/liquidation.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+contract AaveLiquidate is IFlashLoanReceiver{
+    address AaveLendingPool;
+    address UniswapRouter;
+
+    function executeOperation;
+    function LiquidatePositions;
+    function Liquidationcall;
+    function swap;
+    
+}
